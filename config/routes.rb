4e943dccda277e8285e7c563a111c8dad0701a76
Konstantin Dylko <kdylko@gmail.com>
2017-04-13 23:35:01 +0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'store/index'
+
+  resources :products
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "store/index"
+  root to: 'store#index', as: 'store'
+
+end
+
